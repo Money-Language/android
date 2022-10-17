71a2384c971b8972920b82b67c4fe4eefa3211fe
jiwon <ji_1106@naver.com>
2022-10-17 12:05:40 +0900

    [Update] Signup

--- app/src/main/java/com/cmccx/moge/presentation/view/signup/TosFragment.kt
+++ app/src/main/java/com/cmccx/moge/presentation/view/signup/TosFragment.kt
@@ -7,11 +7,17 @@ import com.cmccx.moge.base.BaseFragment
 import com.cmccx.moge.databinding.FragmentTosBinding
 
 class TosFragment : BaseFragment<FragmentTosBinding>(FragmentTosBinding::bind, R.layout.fragment_tos) {
+
+    private var isOtherAgree : Boolean = false
+    private var isAgeAgree : Boolean = false
+    private var isTosAgree : Boolean = false
+    private var isPiAgree : Boolean = false
+    private var isThirdPartyAgree : Boolean = false
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if(checkAgree()) binding.tosNextBtn.visibility = View.VISIBLE
-        else binding.tosNextBtn.visibility = View.GONE
+        checkAgree()
 
         // 뒤로 가기 클릭 시 로그인으로 돌아감
         binding.tosBackIv.setOnClickListener {
@@ -24,8 +30,60 @@ class TosFragment : BaseFragment<FragmentTosBinding>(FragmentTosBinding::bind, R
         }
     }
 
-    // TODO 회원가입 체크박스
-    private fun checkAgree() : Boolean {
-        return true
+    private fun checkAgree() {
+        binding.tosAgreeAllCb.setOnCheckedChangeListener { _, isChecked ->
+            if (!isOtherAgree) {
+                binding.tosAgreeAgeCb.isChecked = isChecked
+                binding.tosAgreeTosCb.isChecked = isChecked
+                binding.tosAgreePiCb.isChecked = isChecked
+                binding.tosAgreeThirdPartyCb.isChecked = isChecked
+
+                isAgeAgree = isChecked
+                isTosAgree = isChecked
+                isPiAgree = isChecked
+                isThirdPartyAgree = isChecked
+            }
+            controlNextButton()
+        }
+
+        binding.tosAgreeAgeCb.setOnCheckedChangeListener { _, isChecked ->
+            isAgeAgree = isChecked
+            checkAllAgree()
+            controlNextButton()
+        }
+
+        binding.tosAgreeTosCb.setOnCheckedChangeListener { _, isChecked ->
+            isTosAgree = isChecked
+            checkAllAgree()
+            controlNextButton()
+        }
+
+        binding.tosAgreePiCb.setOnCheckedChangeListener { _, isChecked ->
+            isPiAgree = isChecked
+            checkAllAgree()
+            controlNextButton()
+        }
+
+        binding.tosAgreeThirdPartyCb.setOnCheckedChangeListener { _, isChecked ->
+            isThirdPartyAgree = isChecked
+            checkAllAgree()
+            controlNextButton()
+        }
+
+    }
+
+    private fun checkAllAgree() {
+        if (isAgeAgree && isTosAgree && isPiAgree && isThirdPartyAgree) binding.tosAgreeAllCb.isChecked = true
+        else if (!(isAgeAgree || isTosAgree || isPiAgree || isThirdPartyAgree)) binding.tosAgreeAllCb.isChecked = false
+        else {
+            isOtherAgree = true
+            binding.tosAgreeAllCb.isChecked = false
+            isOtherAgree = false
+        }
+    }
+
+    private fun controlNextButton() {
+        if (isAgeAgree && isTosAgree && isPiAgree) binding.tosNextBtn.visibility = View.VISIBLE
+        else binding.tosNextBtn.visibility = View.GONE
     }
 }
