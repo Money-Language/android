064f3ed130a1c20f0d77ae32e9898ebe9552bfa8
Hanseul Lee <70684334+HS0204@users.noreply.github.com>
2022-10-20 14:27:35 +0900

    채팅 UI

---
+++ app/src/main/java/com/cmccx/moge/presentation/view/chatting/ChattingFragment.kt
@@ -0,0 +1,8 @@
+package com.cmccx.moge.presentation.view.chatting
+
+import com.cmccx.moge.R
+import com.cmccx.moge.base.BaseFragment
+import com.cmccx.moge.databinding.FragmentChattingBinding
+
+class ChattingFragment: BaseFragment<FragmentChattingBinding>(FragmentChattingBinding::bind, R.layout.fragment_chatting) {
+}
